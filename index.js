c24b6305ee68f63caf68ab6d844ff1701b05d044
pawan suthar <35306329+pawan187@users.noreply.github.com>
2022-04-20 13:59:39 +0530

    main page updated

--- index.js
+++ index.js
@@ -11,5 +11,25 @@
 const { ApolloServer, PubSub } = require("apollo-server");
 const mongoose = require("mongoose");
 const typeDefs = require("./graphql/typeDefs");
-
 const resolvers = require("./graphql/resolvers");
+const { MONGODB } = require("./config.js");
+
+const pubsub = new PubSub();
+
+const PORT = process.env.port || 5000;
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({ req }) => ({ req, pubsub })
+});
+
+mongoose
+  .connect(MONGODB, { useNewUrlParser: true })
+  .then(() => {
+    console.log("MongoDb Connected");
+    return server.listen({ port: PORT });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
