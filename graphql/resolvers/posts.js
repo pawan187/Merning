33dfa4f54fba6521a074bc2b28dd1b12113e7d11
pawan suthar <35306329+pawan187@users.noreply.github.com>
2022-04-20 11:36:55 +0530

    Post apis have been created

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -45,6 +45,43 @@ module.exports = {
         newPost: post
       });
       return post;
+    },
+    async deletePost(_, { postId }, context) {
+      const user = checkAuth(context);
+
+      try {
+        const post = await Post.findById(postId);
+        if (user.username === post.username) {
+          await post.delete();
+          return "Post deleted successfully";
+        } else {
+          throw new AuthenticationError("Action not allowed");
+        }
+      } catch (err) {
+        throw new Error(err);
+      }
+    },
+    async likePost(_, { postId }, context) {
+      const { username } = checkAuth(context);
+      const post = await Post.findById(postId);
+
+      if (post) {
+        if (post.likes.find((like) => like.username === username)) {
+          post.likes = post.like.filter((like) => like.username !== username);
+        } else {
+          post.like.push({
+            username,
+            createdAt: new Date().toISOString()
+          });
+        }
+        await post.save();
+        return post;
+      } else throw new UserInputError("Post not found");
+    }
+  },
+  Subscription: {
+    newPost: {
+      subscribe: (_, __, { pubsub }) => pubsub.asyncIterator("NEW_POST")
     }
   }
 };
